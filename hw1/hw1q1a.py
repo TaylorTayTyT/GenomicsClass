4f4c03938d73feead38a105a3ab7b29e4406c3bc
TaylorTayTyT <taylornguyen904@gmail.com>
2024-01-28 12:18:53 -0500

    finished hq1q1a

--- hw1/hw1q1a.py
+++ hw1/hw1q1a.py
@@ -1,7 +1,15 @@
-import fileinput
+import sys
 
-lines_of_data = fileinput.input()
-#print(type(lines_of_data))  # fileinput.FileInput
+lines_of_data = sys.stdin.readlines()
 
+ans = ""
+header = ">SEQ_ID\n"
+
+ind = 0
 for line in lines_of_data:
-    print(line)
+    if(ind % 4 == 1):
+        ans += header
+        ans += line
+    ind += 1
+
+sys.stdout.write(ans)
